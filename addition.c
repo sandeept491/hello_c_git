87569594d046be545f2636c4a52b01d85091a4d9
sandeept491 <sandeept491@gmail.com>
2016-02-25 10:19:18 +0530

    addition pro

---
+++ addition.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+ clrscr();
+ int a,b,sum;
+ printf("Enter a anb b values \n");
+ scanf("%d %d", &a,&b);
+ sum=a+b;
+ printf("Addition of the two numbers is %d", sum);
+ getch();
+}
